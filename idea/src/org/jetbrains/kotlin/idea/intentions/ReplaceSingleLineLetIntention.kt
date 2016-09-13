244c147585728d6005d8ae354f84c2de63261b59
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-09-13 14:20:50 +0300

    IntentionBasedInspection minor refactoring: inspectionRange --> inspectionTarget

--- idea/src/org/jetbrains/kotlin/idea/intentions/ReplaceSingleLineLetIntention.kt
+++ idea/src/org/jetbrains/kotlin/idea/intentions/ReplaceSingleLineLetIntention.kt
@@ -30,10 +30,7 @@ import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameUnsafe
 
 class ReplaceSingleLineLetInspection : IntentionBasedInspection<KtCallExpression>(ReplaceSingleLineLetIntention::class) {
-    override fun inspectionRange(element: KtCallExpression) = element.calleeExpression?.let {
-        val start = it.getStartOffsetIn(element)
-        TextRange(start, start + it.endOffset - it.startOffset)
-    }
+    override fun inspectionTarget(element: KtCallExpression) = element.calleeExpression
 }
 
 class ReplaceSingleLineLetIntention : SelfTargetingOffsetIndependentIntention<KtCallExpression>(
